df9259d93261569735d313def6d101c60a22506b
Mike Brooks <mike@snp.com>
2023-10-27 21:33:41 +0000

    Challenge in progress

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+# write 'hello world' to the console
+print("hello world")
+
+# prompt the user input one of  three options: rock, paper, or scissors.
+user_choice = input("Do you choose rock, paper or scissors?")
+
+# print the user's choice
+print("user choice: " + user_choice.lower())  
+
+
+
